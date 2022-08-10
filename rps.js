5381f404e893572f5bd941ae047e859f37cd439a
RKGazelle <jpitt8686@gmail.com>
2022-08-10 19:52:40 -0400

    Add updated game logic & finalize UI

--- rps.js
+++ rps.js
@@ -1,22 +1,38 @@
-const rockButton = document.querySelector('#rock');
-const scissorsButton = document.querySelector('#scissors');
-const paperButton = document.querySelector('#paper');
+//initialize variables & event listener constants
+const buttonBox = document.querySelector('.buttonBox');
+const startButton = document.querySelector('#start');
+const resetButton = document.querySelector('#reset');
 const resultsDiv = document.querySelector('#results');
+const introP = document.querySelector('.intro');
+const rockButton = document.createElement('button');
+const scissorsButton = document.createElement('button');
+const paperButton = document.createElement('button');
+
+rockButton.setAttribute('id', 'rock');
+rockButton.textContent = "Rock";
+scissorsButton.setAttribute('id', 'scissors');
+scissorsButton.textContent = "Scissors";
+paperButton.setAttribute('id', 'paper');
+paperButton.textContent = "Paper";
+
 let playerScore = 0;
 let compScore = 0;
 let result = "";
+let outcome = 0;
+
+startButton.addEventListener('click', initGame);
 
 rockButton.addEventListener('click', function() {
   result = playRound("rock",getComputerChoice());
-  resultsDiv.textContent = `${result} Current Score - Player ${playerScore} | Computer ${compScore}`;
+  outcome = updateScore(result);
 });
 paperButton.addEventListener('click', function() {
   result = playRound("paper",getComputerChoice());
-  resultsDiv.textContent = `${result} Current Score - Player ${playerScore} | Computer ${compScore}`;
+  outcome = updateScore(result);
 });
 scissorsButton.addEventListener('click', function() {
   result = playRound("scissors",getComputerChoice());
-  resultsDiv.textContent = `${result} Current Score - Player ${playerScore} | Computer ${compScore}`;
+  outcome = updateScore(result);
 });
 
 function getComputerChoice() {
@@ -61,37 +77,51 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-function game() {
-  
-  let playerScore = 0;
-  let compScore = 0;
-
-  for (i=0; i < 5; i++) {
-    playerInput = prompt("Input your play:")
-    result = playRound(playerInput.toLowerCase(), getComputerChoice());
-    switch (result) {
-      case "You win!":
-        playerScore += 1;
-        console.log(`You won! Your score is now ${playerScore}`)
-        break;
-      case "You lose!":
-        compScore += 1;
-        console.log(`You lost! Computer score is now ${compScore}`)
-        break
-      case "It's a tie!":
-        console.log("It's a tie- no score change.")
-        break
+function updateScore(result) {
+  if(playerScore < 5 && compScore < 5) {
+    resultsDiv.textContent = `${result} Current Score - Player ${playerScore} | Computer ${compScore}`;
+    return 0; // Game() Continues
+    } else if (playerScore === 5) {
+      resultsDiv.textContent = `YOU WIN!!! Final Score - Player: ${playerScore} | Computer: ${compScore}`;
+      resultsDiv.style.cssText = 'background: white; border: 5px solid green; color: green; padding: 20px; text-align: center;';
+      resetGame();
+      return 1; // Player Wins!
+    } else if (compScore === 5) {
+      resultsDiv.textContent = `You lost... Final Score - Player: ${playerScore} | Computer: ${compScore}`;
+      resultsDiv.style.cssText = 'background: white; border: 5px solid red; color: red; padding: 20px; text-align: center;';
+      resetGame();
+      return 2; // Computer Wins!
     }
-  }
+}
 
-  if (playerScore > compScore) {
-    console.log(`You win! Player Score: ${playerScore} | Computer Score: ${compScore}`);
-  } else if (playerScore == compScore) {
-    console.log(`It's a tie! Player Score: ${playerScore} | Computer Score: ${compScore}`)
-  } else {
-    console.log(`You lost... Player Score: ${playerScore} | Computer Score: ${compScore}`);
-  }
+function initGame() {
+  resultsDiv.textContent = "Current Score - Player 0 | Computer 0";
+  playerScore = 0;
+  compScore = 0;
 
-  return playerScore;
+  startButton.remove();
+  buttonBox.append(rockButton, paperButton, scissorsButton);
+  resultsDiv.style.cssText = 'background: gray; color: white; padding: 20px; font-size: 20px; text-align: center;'
+
+  introP.textContent = `W: ${wins} L: ${losses}`;
+
+  if(outcome === 1) {
+    wins++;
+  } else if (outcome === 2) {
+    losses++;
+  } 
+
+
+
+  return;
+}
+
+function resetGame() {
+  rockButton.remove();
+  scissorsButton.remove();
+  paperButton.remove();
+  buttonBox.append(startButton);
+
+  return;
 }
 
